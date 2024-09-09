09ce1e86361758fb929a88816a2fabfb4cbc5a83
ZurielOwsla <ZurielOwsla@gmail.com>
2024-09-09 15:56:24 -0600

    eliminar guiones del UUID para que MySQL lo acepte

--- src/Component/generadorUUID.ts
+++ src/Component/generadorUUID.ts
@@ -2,7 +2,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 // Función que genera un UUID sin guiones
 export const generarUUID = (): string => {
-  const uuidSinGuiones = uuidv4().replace(/-/g, '');  // Elimina los guiones del UUID
-  console.log(`UUID generado sin guiones: ${uuidSinGuiones}`);  // Agregar log para depuración
+  const uuidSinGuiones = uuidv4().replace(/-/g, '');  // Eliminamos los guiones que nos genera el UUID
+  console.log(`UUID generado sin guiones: ${uuidSinGuiones}`);  // Agregar log para depuración 
   return uuidSinGuiones;
 };
